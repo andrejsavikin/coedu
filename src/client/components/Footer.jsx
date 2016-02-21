6a22f0df5fdc2b1cb28d10229572130cf244857b
rista404 <risticn6@gmail.com>
2016-02-21 19:13:24 +0100

    Populating user info in footer

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router';
 
 export default class Footer extends React.Component {
 render() {
+	let user = {
+		fullName: "Nikola Ristić",
+		username: "rista404",
+		team: "III/12",
+		organization: "ETŠ Nikola Tesla"
+	};
     return (
       <footer>
       	<Link to={`/`}>
       	  <img className="logout" src={"images/logout.svg"} />
       	</Link>
-      	<span className="user-info">Nikola Ristić ¬ III/12 ¬ ETŠ Nikola Tesla</span>
+      	<span className="user-info">{ user.fullName } ¬ { user.team } ¬ { user.organization }</span>
       </footer>
     );
   }
