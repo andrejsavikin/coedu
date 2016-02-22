b459b4f9cd9510617370a2378a5134f983995cf1
rista404 <risticn6@gmail.com>
2016-02-22 23:40:27 +0100

    Changed the login function name

--- src/client/components/GithubButton.jsx
+++ src/client/components/GithubButton.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router';
 export default class GithubButton extends React.Component {
 	static propTypes = {
 		cta: React.PropTypes.string.isRequired,
-		clickMethod: React.PropTypes.func.isRequired
+		handleClick: React.PropTypes.func.isRequired
 	};
 
 	render() {
 		return (
-			<div className="GithubButton" onClick={this.props.clickMethod}>
+			<div className="GithubButton" onClick={this.props.handleClick}>
 				<img className="GithubButton__Icon" src={"images/github-white.svg"} />
 				<span className="GithubButton__Text">{this.props.cta}</span>
 			</div>
--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import auth from '../auth.jsx';
+
 import GithubButton from './GithubButton.jsx';
 
 export default class Login extends React.Component {
 
-	handleClick() {
+	handleLogin() {
 		// Refactor
 		auth.login((response) => location.href="#/repos" );
 	}
@@ -13,7 +14,7 @@ export default class Login extends React.Component {
 		return (
 			<div className="Login View">
 				<img className="OctocatImage" src={"./images/octobiwan.jpg"} />
-				<GithubButton cta="Sign In" clickMethod={this.handleClick}/>
+				<GithubButton cta="Sign In" handleClick={this.handleLogin}/>
 			</div>
 		);
 	}
